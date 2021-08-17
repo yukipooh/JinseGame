3ce94d85b9144f28828ee6016cb1b3b21f0a022a
yuki_pooh <yuki@pooh-san.no-ip.com>
2021-08-17 14:09:16 +0900

    一旦むずいから接続するのやめる

---
+++ JinseiGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> playerCars = new List<GameObject>();
+}
--- JinseiGame/Assets/Scripts/SampleScene.cs
+++ JinseiGame/Assets/Scripts/SampleScene.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 // MonoBehaviourPunCallbacksを継承して、PUNのコールバックを受け取れるようにする
 public class SampleScene : MonoBehaviourPunCallbacks
 {
+    [SerializeField] GameManager gameManager;
+
     private void Start() {
         // PhotonServerSettingsの設定内容を使ってマスターサーバーへ接続する
-        PhotonNetwork.ConnectUsingSettings();
+        // PhotonNetwork.ConnectUsingSettings();
     }
 
     // マスターサーバーへの接続が成功した時に呼ばれるコールバック
@@ -19,7 +21,7 @@ public class SampleScene : MonoBehaviourPunCallbacks
     // ゲームサーバーへの接続が成功した時に呼ばれるコールバック
     public override void OnJoinedRoom() {
         // ランダムな座標に自身のアバター（ネットワークオブジェクト）を生成する
-        var position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
-        PhotonNetwork.Instantiate("Car", position, Quaternion.identity);
+        // var position = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
+        // gameManager.playerCars.Add(PhotonNetwork.Instantiate("Car", position, Quaternion.identity)); 
     }
 }
