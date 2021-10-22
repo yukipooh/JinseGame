ef768fec8b4dc7f27aee0e02e988b5fc47e99304
yuki_pooh <yuki@pooh-san.no-ip.com>
2021-10-22 20:32:49 +0900

    ルーレット画像の変更

--- JinseiGame/Assets/Scripts/RouletteMaker.cs
+++ JinseiGame/Assets/Scripts/RouletteMaker.cs
@@ -10,15 +10,15 @@ public class RouletteMaker : MonoBehaviour {
     [SerializeField] private Image rouletteImage;
     [SerializeField] private RouletteController rController;
     public void Initialize () {
-        float ratePerRoulette = 1 / (float) choices.Count;
+        // float ratePerRoulette = 1 / (float) choices.Count;
         float rotatePerRoulette = 360 / (float) (choices.Count);
-        for (int i = 0; i < choices.Count; i++) {
-            var obj = Instantiate (rouletteImage, imageParentTransform);
-            obj.color = rouletteColors[(choices.Count - 1 - i)];
-            obj.fillAmount = ratePerRoulette * (choices.Count - i);
-            obj.GetComponentInChildren<Text> ().text = choices[(choices.Count - 1 - i)];
-            obj.transform.GetChild (0).transform.rotation = Quaternion.Euler (0, 0, ((rotatePerRoulette / 2) + rotatePerRoulette * i));
-        }
+        // for (int i = 0; i < choices.Count; i++) {
+        //     var obj = Instantiate (rouletteImage, imageParentTransform);
+        //     obj.color = rouletteColors[(choices.Count - 1 - i)];
+        //     obj.fillAmount = ratePerRoulette * (choices.Count - i);
+        //     obj.GetComponentInChildren<Text> ().text = choices[(choices.Count - 1 - i)];
+        //     obj.transform.GetChild (0).transform.rotation = Quaternion.Euler (0, 0, ((rotatePerRoulette / 2) + rotatePerRoulette * i));
+        // }
         rController.SetRoulette();
         rController.rMaker = this;
         rController.rotatePerRoulette = rotatePerRoulette;
